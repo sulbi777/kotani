647d8a3578f1ec849600e9532ce142597288c7d3
ISWAHYUDI <45387502+sulbi777@users.noreply.github.com>
2026-07-24 20:18:00 +0700

    Update app.py

--- kotani-backend/app.py
+++ kotani-backend/app.py
@@ -40,5 +40,9 @@ def calculate_route():
 
     return jsonify({"route_action": route_decision, "risk": food_loss_risk}), 200
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    host = os.getenv('HOST', '127.0.0.1')
+    port = int(os.getenv('PORT', 8080))
+    app.run(host=host, port=port)
